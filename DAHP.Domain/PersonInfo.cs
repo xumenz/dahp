e2caf6c87214bb1d3918b3164fc07361ed597ef8
xumenz <xumenz@gmail.com>
2016-11-11 21:42:07 +0800

    11112016_1441 update

--- DAHP.Domain/PersonInfo.cs
+++ DAHP.Domain/PersonInfo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DAHP.Domain
 {
-    public class Class1
+    public class PersonInfo
     {
     }
 }
--- DAHP.Prototype/frmShell.Designer.cs
+++ DAHP.Prototype/frmShell.Designer.cs
@@ -30,10 +30,15 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.taskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newEmploymentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.taskToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(984, 24);
@@ -48,6 +53,20 @@
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
+            // taskToolStripMenuItem
+            // 
+            this.taskToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newEmploymentToolStripMenuItem});
+            this.taskToolStripMenuItem.Name = "taskToolStripMenuItem";
+            this.taskToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.taskToolStripMenuItem.Text = "Task";
+            // 
+            // newEmploymentToolStripMenuItem
+            // 
+            this.newEmploymentToolStripMenuItem.Name = "newEmploymentToolStripMenuItem";
+            this.newEmploymentToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.newEmploymentToolStripMenuItem.Text = "New Employment";
+            // 
             // frmShell
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -60,6 +79,8 @@
             this.Name = "frmShell";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dakkada HR and Payroll Management System";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,6 +90,8 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem taskToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newEmploymentToolStripMenuItem;
     }
 }
 
