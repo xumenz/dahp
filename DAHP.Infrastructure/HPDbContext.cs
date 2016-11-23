881a1243c08196a93c907ee05eae6db3d1598c37
Dominic Thompson <onlythompson@gmail.com>
2016-11-23 13:46:34 +0100

    23112016_1346 update

---
+++ DAHP.Infrastructure/HPDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAHP.Infrastructure
+{
+    public class HPDbContext : DbContext
+    {
+        public HPDbContext():base("name=HPDbContext")
+        {
+
+        }
+
+
+    }
+}
---
+++ DAHP.Infrastructure/Repositories/PersonRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAHP.Infrastructure.Repositories
+{
+    public class PersonRepository
+    {
+        //This will manage an in-memory collection of objects pertaining to the person class
+    }
+}
