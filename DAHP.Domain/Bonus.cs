ee8872a223c7366aafa89c82d04f826c7b1e1593
Victorumoren <victorumoren888@gmail.com>
2016-11-23 15:20:42 +0100

    23112016_1520 Update

---
+++ DAHP.Domain/Bonus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAHP.Domain
+{
+    public class Bonus
+    {
+        public Guid BonusId { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+    }
+}
