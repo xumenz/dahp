8c64d34b2c86d23377de212b9a845d2873fe1bcf
xumenz <xumenz@gmail.com>
2016-11-24 22:19:36 +0800

    22112016_319update

---
+++ DAHP.Domain/QueryGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAHP.Domain
+{
+    public class QueryGenerator
+    {
+        public Guid QueryId { get; set; }
+    }
+}
