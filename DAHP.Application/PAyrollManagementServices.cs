274b7b2cc2474781f80ad193133c782124751245
hemzereen <hemzereenstudies@gmail.com>
2016-11-25 23:10:06 +0100

    25112016 11:08pm Update

--- DAHP.Application/PAyrollManagementServices.cs
+++ DAHP.Application/PAyrollManagementServices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAHP.Domain;
+using DAHP.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,16 @@ namespace DAHP.Application
 {
     class PayrollManagementServices
     {
+        HPDbContext context;
+
+        public PayrollManagementServices()
+        {
+            context = new HPDbContext();
+        }
+        public Guid createEmployeeTypeId(PaymentSchedule PaymentSchedule)
+        {
+            context.PaymentSchedules.Add(PaymentSchedule);
+        }
     }
+        
 }
